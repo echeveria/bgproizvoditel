7ce1862f4eab20729355c470dced961c80b1d0a1
echeveria <s.p.germanov@gmail.com>
2012-01-12 17:10:03 +0200

    Update lib/registry.class.php

--- lib/registry.class.php
+++ lib/registry.class.php
@@ -1,28 +1,16 @@
 <?php
-class SingletonRegistry
+class registry
 {       
         
         private static $instance;
-        
-        /**
-         * Object hash map
-         *
-         * @var array
-         */
+       
         private $map;
         
-        /**
-         * Private constructor
-         *
-         */
+        
         private function __construct()
         {}
         
-        /**
-         * Get the single instance
-         *
-         * @return SingletonRegistry
-         */
+        
         public static function getInstance()
         {
                 if(self::$instance === null)
@@ -33,31 +21,18 @@ class SingletonRegistry
                 return self::$instance;
         }
         
-        /**
-         * Get an object by key
-         *
-         * @param string|int $key
-         * @return object
-         */
+       
         public function get($key)
         {
                 return $this->map[$key];
         }
         
-        /**
-         * Set an object by key
-         *
-         * @param string|int $key
-         */
         public function set($key, $object)
         {
                 return $this->map[$key] = $object;
         }
         
-        /**
-         * Disallow cloning
-         *
-         */
+       
         private function __clone()
         {}
 }
